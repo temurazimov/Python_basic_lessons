f8aad5191fddd7430b278bf27b3efefc2f16a993
Temur Azimov <146569084+temurazimov@users.noreply.github.com>
2023-10-02 00:07:27 +0500

    14-dars qo'shildi

---
+++ 14-dars.Lug'at bilan ishlash.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 22:33:27 2023
+
+@author: T480
+"""
+
+# LUG'AT BILAN ISHLASH
+# Muallif: Azimov Temurbek
+
+# car_0 = {'model':'ferrari','rang':'qizil'}
+# print(car_0['model'])
+# print(car_0['rang'])
+
+# en_uz = {'apple':'olma', 'apricot':'shaftoli', 'banana':"banan"}
+# print(en_uz['apple'])
+# print(en_uz['apricot'])
+# print(en_uz['banana'])
+
+# mevalar = {'olma':10000, 'tarvuz':8000, "qovun":10000}
+# print(f"Olma narhi {mevalar['olma']}")
+
+# Lug'atda istalgan ma'lumot turlarini saqlash mumkin
+# talaba_0 = {'ism':'murod olimov', 'yosh':20, 't_yil':2000}
+# print(f"{talaba_0['ism'].title()},\
+#       {talaba_0['t_yil']} - da tug'ilgan,\
+#        {talaba_0['yosh']} yoshda")
+       
+# # Yangi kalit so'z va qiymat qo'shish
+# talaba_0['kurs'] = 4 
+# talaba_0['fakultet'] = 'informatika' 
+# talaba_0['ism'] = 'abdulloh' # biron qiymatni o'zgartirsak agar
+# print(talaba_0)     
+
+# Bo'sh lug'at 
+# talaba_1 = {}
+# talaba_1['ism'] = 'qobil rasulov'
+# talaba_1['kurs'] = 3
+# talaba_1['yosh'] = 20
+# print(talaba_1)
+# # print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+# # Qiymatni yangilash
+# talaba_1['kurs'] = 4
+# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+# Kalit so'z qiymatini o'chirib tashlash
+# talaba_0 = {'ism':'murod olimov', 'yosh':20, 't_yil':2000}
+# print(talaba_0)
+# del talaba_0['yosh']
+# print(talaba_0)
+
+
+# Lug'atlarni bir necha qatorda yozish mumkin
+
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310',
+#     'anvar':'pixel 3xl'
+#     }
+
+# # get() metodi
+# # phone = telefonlar['ali']
+# # print(f"Alining telefoni {phone}")
+
+# # phone =  telefonlar['hasan']
+# # print(f"Hasanning telefoni {phone}")
+# # phone = telefonlar.get('ali',"Bunday ism mavjud emas")
+# # phone = telefonlar.get('hasan',"Bunday ism mavjud emas")
+# phone = telefonlar.get('hasan')
+# print(phone)
+
+# AMALIYOT
